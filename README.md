7bccb7ce6b3163d95c36f96d7d80bde6cbec9438
Niels Meyering <nmeyering@gmail.com>
2013-07-30 16:28:42 +0200

    first i3 integration working

--- README.md
+++ README.md
@@ -1,4 +1,21 @@
 Description
 ===========
 
-This is a little program that monitors network throughput and outputs it continuously in a format that can be used in conjunction with i3bar program to display a textual (ASCII) sparkline graph using [spark](https://github.com/holman/spark).
+This is a little program that monitors network throughput and outputs it continuously in a format that can be used in conjunction with i3bar to display a textual (ASCII) sparkline graph. You will need [spark](https://github.com/holman/spark) for this.
+
+
+In my ~/.config/i3/config:
+```
+bar {
+	status_command network_spark.sh
+	...
+}
+```
+
+In my ~/.config/i3status:
+```
+general {
+	output_format = "none"
+	...
+}
+```
---
+++ network_spark.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+if (( $# > 0 )); then
+	interface="$1"
+else
+	interface="eth0"
+fi
+
+function human_readable()
+{
+	local kilo=$( echo "scale=2; $1 / 1024" | bc )
+	local kiloint=$( echo "$1 / 1024" | bc )
+
+	local mega=$( echo "scale=2; $kilo / 1024" | bc )
+	local megaint=$( echo "$kilo / 1024" | bc )
+
+	local giga=$( echo "scale=2; $mega / 1024" | bc )
+	local gigaint=$( echo "$mega / 1024" | bc )
+
+	local format="%#3d"
+
+	if [ $kiloint -lt 1 ] ; then
+		printf "${format}  B/s" "$1"
+	elif [ $megaint -lt 1 ] ; then
+		printf "${format} KB/s" "$kiloint"
+	elif [ $gigaint -lt 1 ] ; then
+		printf "${format} MB/s" "$megaint"
+	else
+		printf "${format} GB/s" "$gigaint"
+	fi
+}
+
+function get_traffic()
+{
+	# ...by parsing /proc/net/dev
+	grep "${1}" /proc/net/dev | sed "s/.*${1}: \([0-9]\+\).*/\1/"
+}
+
+traffic_old=$(get_traffic "${interface}")
+
+values=(0 0 0 0 0 0 0 0 0 0)
+
+#!/bin/bash
+i3status --config "${HOME}/.config/i3status" | while :
+do
+	read line
+
+	max_length=10
+
+	traffic_new=$(get_traffic "${interface}")
+
+	# difference between the last two measurements is the new value
+	difference=$((${traffic_new} - ${traffic_old}))
+
+	# append the new value to the end of the values array
+	values=("${values[@]}" $difference)
+
+	# update old traffic value
+	traffic_old="${traffic_new}"
+
+	# shift values / dequeue first element
+	values=(${values[@]:1})
+
+	echo "$(human_readable ${difference}) $(spark "${values[@]}") | ${line}" || exit 1
+done
